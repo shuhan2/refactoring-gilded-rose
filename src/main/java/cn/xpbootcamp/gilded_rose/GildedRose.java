b3783e3bce4a51b916b9b794d6cf02441587a379
qianwu2 <qianwu2@ap.xom.com>
2020-04-30 10:48:05 +0800

    glided-rose [qian] simply one expression as 0

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -54,7 +54,7 @@ class GildedRose {
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = 0;
                     }
                 } else {
                     if (items[i].quality < 50) {
