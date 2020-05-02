c7cdf9eafefdf48eb08c0b02352319e41ff453e9
qianwu2 <qianwu2@ap.xom.com>
2020-05-02 17:20:52 +0800

    glided-rose [qian] extract method for  updateQuality and updateSellIn

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -17,36 +17,46 @@ class GildedRose {
   public void updateQualityAndSellInForOneDay() {
 
     for (Item item : items) {
-      if (item.isEqualsName(SULFURAS)) {
-        continue;
-      }
+      updateQuality(item);
+      updateSellIn(item);
+    }
+  }
 
-      if (!item.isEqualsName(AGED_BRIE) && !item.isEqualsName(BACKSTAGE_PASSES)) {
+  private void updateQuality(Item item) {
+    if (item.isEqualsName(SULFURAS)) {
+      return;
+    }
+
+    if (!item.isEqualsName(AGED_BRIE) && !item.isEqualsName(BACKSTAGE_PASSES)) {
+      item.quality = getChangedQuality(item.quality, -1);
+      if (item.sell_in < 0) {
         item.quality = getChangedQuality(item.quality, -1);
-        if (item.sell_in < 0) {
-          item.quality = getChangedQuality(item.quality, -1);
-        }
-      } else {
-        if (item.isEqualsName(BACKSTAGE_PASSES)) {
-          if (item.sell_in < 1) {
-            item.quality = MIN_QUALITY;
-          } else if (item.sell_in < 6) {
-            item.quality = getChangedQuality(item.quality, 3);
-          } else if (item.sell_in < 11) {
-            item.quality = getChangedQuality(item.quality, 2);
-          } else {
-            item.quality = getChangedQuality(item.quality, 1);
-          }
+      }
+    } else {
+      if (item.isEqualsName(BACKSTAGE_PASSES)) {
+        if (item.sell_in < 1) {
+          item.quality = MIN_QUALITY;
+        } else if (item.sell_in < 6) {
+          item.quality = getChangedQuality(item.quality, 3);
+        } else if (item.sell_in < 11) {
+          item.quality = getChangedQuality(item.quality, 2);
         } else {
           item.quality = getChangedQuality(item.quality, 1);
         }
+      } else {
+        item.quality = getChangedQuality(item.quality, 1);
       }
-      if (item.quality > MAX_QUALITY) {
-        item.quality = MAX_QUALITY;
-      }
-      if (item.quality < MIN_QUALITY) {
-        item.quality = MIN_QUALITY;
-      }
+    }
+    if (item.quality > MAX_QUALITY) {
+      item.quality = MAX_QUALITY;
+    }
+    if (item.quality < MIN_QUALITY) {
+      item.quality = MIN_QUALITY;
+    }
+  }
+
+  private void updateSellIn(Item item) {
+    if (!item.isEqualsName(SULFURAS)) {
       item.sell_in = item.sell_in - 1;
     }
   }
